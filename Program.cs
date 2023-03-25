5a01c32d1b4dc487f11307a4bb7263c04af6e0fe
Dmitry <marknmaxm@gmail.com>
2023-03-26 00:13:55 +0300

    Complete. Version 1.0

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
-﻿//
-
-Console.Write("Задайте объём массива строк для анализа (введите число): ");
+﻿Console.Write("Задайте объём массива строк для анализа (введите число): ");
 int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = GetArray(size);
+Console.WriteLine($"Вы создали следующий массив строк: [{String.Join(";", array)}]");
+
+string[] newArray = NewArray(array, 3);
+Console.WriteLine($"Скорректированный массив строк равен: [{String.Join(";", newArray)}]");
 
 
 /// <summary>
 /// Метод для создания одномерного массива строк значения которого задаёт пользователь
 /// </summary>
 /// <param name="size">Задаём размер массива</param>
-/// <returns></returns>
+/// <returns>Одномерный массив строк</returns>
 string[] GetArray(int size)
 {
     Console.WriteLine("Задайте значение элементом массива через ENTER");
@@ -19,12 +23,35 @@ string[] GetArray(int size)
     }
     return array;
 }
-string[] array = GetArray(2);
 
-Console.WriteLine(String.Join(";", array));
+/// <summary>
+/// Метод генерирует новый массив строк из элементов первичного массива, которые не прывашают определённый массив символов
+/// </summary>
+/// <param name="array">Массив для анализа</param>
+/// <param name="count">Количество символов в одном элементе для нового массива</param>
+/// <returns>Одномерный строковый массив из элементов по заданному размеру</returns>
+string[] NewArray(string[] array, int count)
+{
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= count)
+        {
+            number++;
+        }
+    }
 
+    string[] newArray = new string[number];
+    int numberCell = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length <= count)
+        {
+            newArray[numberCell] = array[j];
+            numberCell++;
+        }
+    }
 
-
-
-
+    return newArray;
+}
 
